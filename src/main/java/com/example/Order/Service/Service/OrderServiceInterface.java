777bea7e89059f0b45d89eae1d4a1a90cc515cb4
Aaryan <aaryanuttam54321@gmail.com>
2025-06-20 23:49:37 +0530

    Enabling Discovery Client and Feign Client

--- src/main/java/com/example/Order/Service/Service/OrderServiceInterface.java
+++ src/main/java/com/example/Order/Service/Service/OrderServiceInterface.java
@@ -3,5 +3,5 @@ package com.example.Order.Service.Service;
 import com.example.Order.Service.Entity.Order;
 
 public interface OrderServiceInterface {
-    public String saveOrder(Order order);
+    public Order placeOrder(int quantity, Long userId, Long productId);
 }
