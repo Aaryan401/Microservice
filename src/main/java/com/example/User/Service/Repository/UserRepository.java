0fd5da997e84010cb60ff76f344fe69ec9a0161d
Aaryan <aaryanuttam54321@gmail.com>
2025-06-20 23:36:15 +0530

    Created User Repository

--- src/main/java/com/example/User/Service/Repository/UserRepository.java
+++ src/main/java/com/example/User/Service/Repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.User.Service.Repository;
 
+import com.example.User.Service.Entity.Profile;
 import com.example.User.Service.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
