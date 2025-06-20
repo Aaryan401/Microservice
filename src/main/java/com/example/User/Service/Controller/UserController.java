119336a4c4c8718cb8588169c1283cbc2382fdc4
Aaryan <aaryanuttam54321@gmail.com>
2025-06-20 23:36:53 +0530

    Created a Get and PostMapping to register and Profile

--- src/main/java/com/example/User/Service/Controller/UserController.java
+++ src/main/java/com/example/User/Service/Controller/UserController.java
@@ -1,15 +1,14 @@
 package com.example.User.Service.Controller;
 
+import com.example.User.Service.Entity.Profile;
 import com.example.User.Service.Entity.User;
+import com.example.User.Service.Model.UserDto;
 import com.example.User.Service.Service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/user-service/v1")
@@ -23,5 +22,17 @@ public class UserController {
     public ResponseEntity<String> registerUser(@RequestBody User user){
         String response = userService.saveUser(user);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("create-profile/{userId}")
+    public ResponseEntity<String> createProfile(@RequestBody Profile profile, Long userId) {
+        String response = userService.saveProfile(profile, userId);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("get-user/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        UserDto userDto = userService.findUserById(userId);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 }
