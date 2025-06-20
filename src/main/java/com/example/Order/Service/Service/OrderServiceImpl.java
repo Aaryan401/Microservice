8fc22ca2219c4b4782623f7343c83e17319cecb7
Aaryan <aaryanuttam54321@gmail.com>
2025-06-20 23:51:25 +0530

    Created an Order service to placeOrder

--- src/main/java/com/example/Order/Service/Service/OrderServiceImpl.java
+++ src/main/java/com/example/Order/Service/Service/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.Order.Service.Service;
 
 
+import com.example.Order.Service.Client.ProductClient;
 import com.example.Order.Service.Entity.Order;
+import com.example.Order.Service.Model.ProductDto;
 import com.example.Order.Service.Repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,29 @@ public class OrderServiceImpl implements OrderServiceInterface {
     @Autowired
     private final OrderRepository orderRepository;
 
-    @Override
+    @Autowired
+    private final ProductClient productClient;
+
     public String saveOrder(Order order) {
         orderRepository.save(order);
         return "Order Successfully";
+    }
+
+    @Override
+    public Order placeOrder(int quantity, Long userId, Long productId) {
+        ProductDto product = productClient.getProductById(productId);
+
+        if(product.getQuantity()<quantity){
+            throw new RuntimeException("Insufficient product quantity");
+        }
+        Order order = Order.builder()
+                .productId(product.getProductId())
+                .userId(userId)
+                .quantity(quantity)
+                .totalPrice(product.getPrice()*quantity)
+                .address("Bangalore")
+                .build();
+
+        return orderRepository.save(order);
     }
 }
