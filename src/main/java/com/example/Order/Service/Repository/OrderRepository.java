a1373b37e9358489b24498402cd876d43ab8610e
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:47:58 +0530

    Created two Abstract method in OrderRepository

--- src/main/java/com/example/Order/Service/Repository/OrderRepository.java
+++ src/main/java/com/example/Order/Service/Repository/OrderRepository.java
@@ -2,9 +2,15 @@ package com.example.Order.Service.Repository;
 
 
 import com.example.Order.Service.Entity.Order;
+import com.example.Order.Service.Model.OrderDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    public Optional<Order> findByUserIdAndProductId(Long userId, Long productId);
+    public Optional<List<Order>> findByUserId(Long userId);
 }
