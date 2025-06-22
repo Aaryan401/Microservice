2a7fd0c8270f5ded4c8f0210ec80691371b9a279
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:51:21 +0530

    Added One Handler Method in Product Client

--- src/main/java/com/example/Order/Service/Client/ProductClient.java
+++ src/main/java/com/example/Order/Service/Client/ProductClient.java
@@ -5,10 +5,15 @@ import com.example.Order.Service.Model.ProductDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.Optional;
 
 @FeignClient(name="Product-Service")    //We need to mention the application name in the @FeignClient
 public interface ProductClient {
     @GetMapping("api/product-service/v1/get-specific-product/{pid}")
     public ProductDto getProductById(@PathVariable(name="pid") Long productId);
 
+    @PutMapping("api/product-service/v1/subtract-quantity/{productId}")
+    public void updateProductQuantity(@PathVariable("productId") Long productId, ProductDto product);
 }
