f2b2e06c50155215c19e0232d399b9c46414a95b
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:54:33 +0530

    Created methods to dynamically to Place order, Get the list of all order,Get the List of Order of user

--- src/main/java/com/example/Order/Service/Service/OrderServiceInterface.java
+++ src/main/java/com/example/Order/Service/Service/OrderServiceInterface.java
@@ -1,7 +1,15 @@
 package com.example.Order.Service.Service;
 
 import com.example.Order.Service.Entity.Order;
+import com.example.Order.Service.Model.OrderDto;
+import com.example.Order.Service.Model.PlaceOrderDto;
+
+import java.util.List;
 
 public interface OrderServiceInterface {
     public Order placeOrder(int quantity, Long userId, Long productId);
+    public Order placeOrderDynamically(PlaceOrderDto placeOrderDto, Long userId, Long productId);
+    public List<OrderDto> findAllOrderByUserId(Long userId);
+    public List<OrderDto> findAllOrders();
+    public OrderDto findOrderById(Long userId, Long productId);
 }
