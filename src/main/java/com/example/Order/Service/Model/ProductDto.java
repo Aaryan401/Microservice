f64aef5ad2b670c9434e06070323a22b4fbf422e
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:58:26 +0530

    Give a proper Documentation for ProductDto

--- src/main/java/com/example/Order/Service/Model/ProductDto.java
+++ src/main/java/com/example/Order/Service/Model/ProductDto.java
@@ -1,15 +1,41 @@
 package com.example.Order.Service.Model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
+
+    @Schema(
+            description = "Unique identifier for the product",
+            example = "1"
+    )
     private Long productId;
+    @Schema(
+            description = "Name of the product",
+            example = "Smartphone"
+    )
     private String productName;
+    @Schema(
+            description = "Description of the product",
+            example = "Latest model with advanced features"
+    )
     private String description;
+    @Schema(
+            description = "Available quantity of the product",
+            example = "100"
+    )
     private int quantity;
+    @Schema(
+            description = "Price of the product",
+            example = "299.99"
+    )
     private Double price;
 }
