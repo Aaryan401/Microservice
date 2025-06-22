a5a52d1293ac94fa271ed5b746cc546dbe2c194b
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 21:56:23 +0530

    Enable OpenApiDescription

--- src/main/java/com/example/User/Service/UserServiceApplication.java
+++ src/main/java/com/example/User/Service/UserServiceApplication.java
@@ -1,11 +1,20 @@
 package com.example.User.Service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User Service API",
+				description = "API for managing user profiles in the e-commerce application",
+				version = "1.0.0"
+		)
+)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
