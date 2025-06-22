deb18fd22a603662f5311af7e7d5d944bbca1505
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:15:25 +0530

    Enable OpenApiDescription with swagger

--- src/main/java/com/example/Product/Service/ProductServiceApplication.java
+++ src/main/java/com/example/Product/Service/ProductServiceApplication.java
@@ -1,11 +1,19 @@
 package com.example.Product.Service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Product Service API",
+				version = "1.0",
+				description = "API for managing products in the e-commerce application"
+		))
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
