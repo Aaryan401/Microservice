ae687b05c6a4f9b6cdd681817876def89e24ced5
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:49:50 +0530

    Enable Annotation OpenApiDefinition

--- src/main/java/com/example/Order/Service/OrderServiceApplication.java
+++ src/main/java/com/example/Order/Service/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.Order.Service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +10,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Order Service API",
+				description = "API for managing products in the e-commerce application",
+				version = "1.0.0"
+		)
+)
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
