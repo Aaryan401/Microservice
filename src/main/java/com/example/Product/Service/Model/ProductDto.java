64e393df5f876cf226a462859cade14e3f33de97
Aaryan <aaryanuttam54321@gmail.com>
2025-06-22 22:16:42 +0530

    Give the Proper Description for the Dto

--- src/main/java/com/example/Product/Service/Model/ProductDto.java
+++ src/main/java/com/example/Product/Service/Model/ProductDto.java
@@ -1,5 +1,6 @@
 package com.example.Product.Service.Model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -9,17 +10,28 @@ import lombok.Data;
 @Builder
 public class ProductDto {
 
+    @Schema( description = "Unique identifier for the product",
+            example = "1")
     private Long productId;
+
+    @Schema(description = "Name of the product",
+            example = "Laptop")
 
     @NotBlank(message ="product Name must nt be blank")
     private String productName;
 
+    @Schema(description = "Description of the product",
+            example = "High performance laptop with 16GB RAM and 512GB SSD")
     @NotBlank
     private String description;
 
+    @Schema(description = "Available quantity of the product",
+            example = "50")
    @NotNull(message = "Quantity must not be null")
     private int quantity;
 
+    @Schema(description = "Price of the product",
+            example = "999.99")
    @NotNull(message = "Price mst not be null")
     private Double price;
 }
